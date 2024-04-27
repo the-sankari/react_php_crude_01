06e32dfb7b43100a5e2f5e424d7f3bded975944a
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-04-28 00:30:24 +0300

    Crud app initialization

---
+++ README.md
@@ -0,0 +1,6 @@
+# REACT PHP CRUD WEB APP
+
+A simple crude app made with react, php and mysql localy hosted.
+
+
+## 
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+    <main className="container">
+      <h1>Hello </h1>
+      
+    </main>
+    </>
+  );
+}
+
+export default App;
